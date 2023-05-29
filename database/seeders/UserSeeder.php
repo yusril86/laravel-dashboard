4cb0803a72c49deb29275ccd7104bf5b5b5df075
Yusril Anwar <yusril886@gmail.com>
2023-05-29 15:00:07 +0800

    add seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
